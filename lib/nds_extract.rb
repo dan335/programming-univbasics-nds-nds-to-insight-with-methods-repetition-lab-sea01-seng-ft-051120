969d63925d3d3a15bd2612dfa0d509f58b54ae0c
Daniel Phillips <danphi@gmail.com>
2020-01-24 04:39:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,13 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  diretors = ""
+  i = 0
+  while i < source.size do
+    diretors += source[i][:name]
+    i += 1
+  end
+  directors
 end
 
 def total_gross(source)
